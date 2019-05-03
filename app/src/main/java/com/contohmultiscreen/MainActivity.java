fdbdffeb1e058c9738417a4e4da0e3ad216eedc7
sutriesnazvun <sutriesnazvun@gmail.com>
2019-05-03 17:48:30 +0700

    saya menambahkan komentar di java

--- app/src/main/java/com/contohmultiscreen/MainActivity.java
+++ app/src/main/java/com/contohmultiscreen/MainActivity.java
@@ -3,6 +3,12 @@ package com.contohmultiscreen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * create by sutriesnazvun
+ * free coding
+ * good coding
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
